6bfc2dab0e694fc024a39c578403c9ecc51dc7d4
Esteban Zapata <zapata.e96@gmail.com>
2018-04-19 19:04:52 -0400

    Lots o stuff

--- BeatBlocks.sol
+++ BeatBlocks.sol
@@ -1,24 +1,15 @@
 pragma solidity ^0.4.0;
 
 contract ArtistFactory {
-    struct ArtistInfo {
-        string name;
-        address contractAddress;
-        bool isValid;
-    }
-    
-    mapping(address => ArtistInfo) artists; 
-    address[] public artistList;
+    mapping(address => address) artists; 
+    address[] artistList;
 
-    function createArtist(string _name) public {
-        if (artists[msg.sender].isValid) {
+    function createArtist(string _name, uint _subscriptionPriceInWei, uint _subscriptionLengthInDays) public {
+        if (artists[msg.sender] != address(0)) {
             return;
         }
         
-        var artist = artists[msg.sender];
-        artist.name = _name;
-        artist.contractAddress = new Artist(msg.sender);
-        artist.isValid = true;
+        artists[msg.sender] = new Artist(msg.sender, _name, _subscriptionPriceInWei, _subscriptionLengthInDays);
         artistList.push(msg.sender) -1;
     }
     
@@ -26,82 +17,77 @@ contract ArtistFactory {
         return artistList;
     }
     
-    function getArtist(address _address) view public returns (string, address) {
-        return (artists[_address].name, artists[_address].contractAddress);
+    function getArtist(address _address) view public returns (address) {
+        return artists[_address];
     }
 }
 
 contract Artist {
     address owner;
     address factory;
+    string public name;
+    uint public subscriptionPriceInWei;
+    uint public subscriptionLengthInSeconds;
+    string[] ipfsCollections;
     
-    struct Collection {
-        string name;
-        string[] songIpfsAddresses;
-        uint price;
-        bool isValid;
+    mapping(address => uint) subscriptions;
+    
+    modifier onlyBy(address _account) {
+        require(msg.sender == _account);
+        _;
     }
     
-    struct Song {
-        string name;
-        uint price;
-        bool isValid;
-    }
-    
-    mapping(string => Song) songs;
-    mapping(string => Collection) collections;
-    
-    string[] songList;
-    string[] collectionList;
-    
-    function Artist(address _owner) public {
+    constructor(address _owner, string _name, uint _subscriptionPriceInWei, uint _subscriptionLengthInSeconds) public {
         owner = _owner;
         factory = msg.sender;
+        name = _name;
+        subscriptionPriceInWei = _subscriptionPriceInWei;
+        subscriptionLengthInSeconds = _subscriptionLengthInSeconds;
     }
     
-    function createSong(string _ipfsAddress, string _name, uint _price) public {
-        if (songs[_ipfsAddress].isValid) {
-            return;
+    function updateName(string _name) public onlyBy(owner) {
+        name = _name;
+    }
+    
+    function updateSubscriptionPrice(uint _subscriptionPriceInWei) public onlyBy(owner) {
+        subscriptionPriceInWei = _subscriptionPriceInWei;
+    }
+    
+    function updateSubscriptionLength(uint _subscriptionLengthInSeconds) public onlyBy(owner) {
+        subscriptionLengthInSeconds = _subscriptionLengthInSeconds;
+    }
+    
+    function subscribe() public payable {
+        require(msg.value == subscriptionPriceInWei);
+        
+        owner.transfer(msg.value);
+        subscriptions[msg.sender] = now;
+    }
+    
+    function getIpfsCollectionCount() public view returns (uint) {
+        return ipfsCollections.length;
+    }
+    
+    function getIpfsCollection(uint _index) public view returns (string) {
+        return ipfsCollections[_index];
+    }
+    
+    function addIpfsCollection(string _hash) public onlyBy(owner) {
+        ipfsCollections.push(_hash);
+    }
+    
+    function removeIpfsCollection(uint _index) public onlyBy(owner) {
+        if (_index >= ipfsCollections.length) return;
+
+        for (uint i = _index; i<ipfsCollections.length-1; i++){
+            ipfsCollections[i] = ipfsCollections[i+1];
         }
         
-        var song = songs[_ipfsAddress];
-        song.name = _name;
-        song.price = _price;
-        song.isValid = true;
-        
-        songList.push(_ipfsAddress) -1;
+        delete ipfsCollections[ipfsCollections.length-1];
+        ipfsCollections.length--;
     }
     
-    function createCollection(string _name, uint _price) public {
-        if (collections[_name].isValid) {
-            return;
-        }
-        
-        var collection = collections[_name];
-        collection.name = _name;
-        collection.price = _price;
-        
-        collectionList.push(_name) -1;
+    function getUserSubscriptionTimestamp(address _user) public view returns (uint) {
+        return subscriptions[_user];
     }
-    
-    function addSongToCollection(string _songIpfs, string _collectionName) public {
-        collections[_collectionName].songIpfsAddresses.push(_songIpfs) -1;
-    }
-    
-    /*
-    function getSongs() public view returns (string[]) {
-        return songList;
-    }
-    
-    function getSong(string _ipfsAddress) public view returns(string, uint) {
-        return (songs[_ipfsAddress].name, songs[_ipfsAddress].price);
-    }
-    
-    function getCollections() public view returns (string[]) {
-        return collectionList;
-    }
-    
-    function getCollection(string _name) public view returns(string, string[], uint) {
-        return (collections[_name].name, collections[_name].songIpfsAddresses, collections[_name].price);
-    }*/
 }
